21156e5f1a91bdecb1a07679882d5e94e79da4c9
aliwimo <aliwimo@gmail.com>
2020-12-27 00:05:42 +0300

    added .gitignore added python implementation

---
+++ python/benchmark.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+# ========================================================
+# Sphere Function
+# Dimensions: d, xi = [-5.12, 5.12], for all i = 1, .., d.
+# Global minimum 0 at (0, ..., 0)
+# ========================================================
+def sphere(X):
+    return sum(x ** 2 for x in X)
+
+# ========================================================
+# Bohchevsky Function
+# Dimensions: 2, xi = [-100, 100], for all i = 1, .., d.
+# Global minimum 0 at (0, ..., 0)
+# ========================================================
+def Bohchevsky(X):
+    x1 = X[0]
+    x2 = X[1]
+    eq = (x1 ** 2) + 2 * (x2 ** 2) - 0.3 * np.cos((3 * np.pi * x1) + (4 * np.pi * x2)) + 0.3
+    return eq
+
+# ========================================================
+# Booth Function
+# Dimensions: 2, xi = [-10, 10], for all i = 1, .., d.
+# Global minimum 0 at (1, 3)
+# ========================================================
+def Booth(X):
+    x1 = X[0]
+    x2 = X[1]
+    eq = ((x1 + 2 * x2 - 7) ** 2) + ((2 * x1 + x2 - 5) ** 2)
+    return eq
+
+# ========================================================
+# Drop-Wave Function
+# Dimensions: 2, xi = [-5.12, 5.12], for all i = 1, 2.
+# Global minimum -1 at (0, 0)
+# ========================================================
+def drop_wave(X):
+    up = 1 + np.cos(12 * np.sqrt((X[0] ** 2) + (X[1] ** 2)))
+    down = 0.5 * ((X[0] ** 2) + (X[1] ** 2)) + 2
+    return -up / down
+
+
+# ========================================================
+# Eggholder Function
+# Dimensions: 2, xi = [-512, 512], for all i = 1, 2.
+# Global minimum -959.6407 at (512, 404.2319)
+# ========================================================
+def eggholder(X):
+    x1 = X[0]
+    x2 = X[1]
+    eq1 = -(x2 + 47) * np.sin(np.sqrt(abs(x2 + (x1 / 2) + 47)))
+    eq2 = x1 * np.sin(np.sqrt(abs(x1 - (x2 + 47))))
+    return eq1 - eq2
+
+
+
+
+def schwefel_2_22(X):
+    abs_list = list(map(abs, X))
+    inner_sum = np.sum(abs_list)
+    inner_prod = np.prod(abs_list)
+    return inner_sum + inner_prod
---
+++ python/ipa.py
@@ -0,0 +1,142 @@
+import numpy as np
+from random import choice, random
+import benchmark
+
+POP_SIZE    = 30
+DIM_SIZE    = 30
+T_MAX       = 200000
+T_CR        = POP_SIZE
+BOUND       = 10
+LB          = -BOUND
+UB          = BOUND
+NoD         = 1
+NoR         = 3
+OBJ_FUNC    = benchmark.schwefel_2_22
+
+
+# generate population
+def generate_population():
+    return np.random.uniform(0, 1, (POP_SIZE, DIM_SIZE)) * (UB - LB) + LB
+
+# calculate fitness
+def fitness(x):
+    return OBJ_FUNC(x)
+
+# infect individual
+def infect(x_k, x_m):
+    j = np.random.randint(0, DIM_SIZE)
+    x_k[j] += np.random.uniform(-1.0, 1.0) * (x_k[j] - x_m[j])
+    return x_k
+
+# plasma donation to infected indiviual
+def give_plasma(x_k_rcv, x_m_dnr):
+    for j in range(DIM_SIZE):
+        x_k_rcv[j] += np.random.uniform(-1.0, 1.0) * (x_k_rcv[j] - x_m_dnr[j])
+    return x_k_rcv
+
+
+# get lists of indexes of donors and recievers
+def get_donors_recievers(fitnesses):
+    d_indexes = []
+    r_indexes = []
+    sorted_indexes = np.argsort(fitnesses)
+    for i in range(NoD):
+        d_indexes.append(sorted_indexes[i])
+    for i in range(NoR):
+        r_indexes.append(sorted_indexes[-1 - i])
+    return d_indexes, r_indexes
+
+# update donor with equation 5
+def update_donor(x_m_dnr):
+    for j in range(DIM_SIZE):
+        x_m_dnr[j] += np.random.uniform(-1.0, 1.0) * x_m_dnr[j]
+    return x_m_dnr
+
+
+population = generate_population()
+fitnesses = [fitness(i) for i in population]
+
+x_best_index = fitnesses.index(min(fitnesses))
+x_best = population[x_best_index].copy()
+x_best_fit = fitness(x_best)
+
+
+while T_CR < T_MAX:
+
+    # infection distribution
+    for k in range(POP_SIZE):
+        if T_CR < T_MAX:
+            T_CR += 1
+            m = np.random.randint(0, POP_SIZE)
+            while m == k:
+                m = np.random.randint(0, POP_SIZE)
+            x_k = population[k].copy()
+            x_m = population[m].copy()
+            x_k_inf = infect(x_k, x_m)
+            x_k_inf_fit = fitness(x_k_inf)
+            if (x_k_inf_fit < fitnesses[k]):
+                population[k] = x_k_inf.copy()
+                fitnesses[k] = x_k_inf_fit
+                if x_k_inf_fit < x_best_fit:
+                    x_best = x_k_inf.copy()
+                    x_best_fit = x_k_inf_fit
+                    print(x_best_fit)
+        else:
+            break
+    
+    # plasma transfer
+    dose_control = np.ones(NoR)
+    d_indexes, r_indexes = get_donors_recievers(fitnesses)
+    treatment_control = np.ones(NoR)
+    for i in range(NoR):
+        k = r_indexes[i]
+        m = choice(d_indexes)            
+        while treatment_control[i] == 1:
+            if T_CR < T_MAX:
+                T_CR += 1
+                x_k_rcv_p = give_plasma(population[k], population[m])
+                x_k_rcv_p_fit = fitness(x_k_rcv_p)
+                if dose_control[i] == 1:
+                    if x_k_rcv_p_fit < fitnesses[m]:
+                        dose_control[i] += 1
+                        # here may be an error
+                        population[k] = x_k_rcv_p.copy()
+                        fitnesses[k] = x_k_rcv_p_fit
+                    else:
+                        population[k] = population[m].copy()
+                        fitnesses[k] = fitnesses[m]
+                        treatment_control[i] = 0
+                else:
+                    if x_k_rcv_p_fit < fitnesses[k]:
+                        population[k] = x_k_rcv_p.copy()
+                        fitnesses[k] = x_k_rcv_p_fit
+                    else:
+                        treatment_control[i] = 0
+                
+                if fitnesses[k] < x_best_fit:
+                    x_best = population[k].copy()
+                    x_best_fit = fitnesses[k]
+                    print(x_best_fit)
+            else:
+                break
+
+    # Donor update
+    for i in range(NoD):
+        if T_CR < T_MAX:
+            T_CR += 1
+            m = d_indexes[i]
+            x_m_dnr = population[m].copy()
+            if (T_CR / T_MAX) < random():
+                x_m_dnr = update_donor(x_m_dnr)
+                population[m] = x_m_dnr.copy()
+            else:
+                population[m] = np.random.uniform(0, 1, (DIM_SIZE)) * (UB - LB) + LB
+            fitnesses[m] = fitness(population[m])
+            
+            if fitnesses[m] < x_best_fit:
+                x_best = population[m].copy()
+                x_best_fit = fitnesses[m]
+                print(x_best_fit)
+        else:
+            break
+            
