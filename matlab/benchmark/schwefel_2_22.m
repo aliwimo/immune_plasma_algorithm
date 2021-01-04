413141b6e7b4a148a6f5f926e584b420a6bea0eb
aliwimo <aliwimo@gmail.com>
2021-01-05 00:25:52 +0300

    added schwefel2.22 function

---
+++ matlab/benchmark/schwefel_2_22.m
@@ -0,0 +1,11 @@
+function fit = schwefel_2_22(x)
+    dimension_size = length(x);
+    total_sum = 0;
+    total_mul = 1;
+    for i = 1:dimension_size
+        total_sum = total_sum + abs(x(i));
+        total_mul = total_mul * abs(x(i));
+    end
+    fit = total_sum + total_mul;
+end
+
