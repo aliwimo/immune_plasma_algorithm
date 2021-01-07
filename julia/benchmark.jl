4eada68fe1d9119be71292a061ac94ae80cb1e6f
aliwimo <aliwimo@gmail.com>
2021-01-08 01:09:16 +0300

    added ackley function

--- julia/benchmark.jl
+++ julia/benchmark.jl
@@ -1,5 +1,6 @@
 
-
+# bounds +/- 100
+# maximum evaluations 150,000
 function sphere(x)
     D = length(x)
     sum = 0
@@ -7,4 +8,22 @@ function sphere(x)
         sum += (x[i]^2)
     end
     return sum
+end
+
+# bounds +/- 32
+# maximum evaluations 150,000
+function ackley(x)
+    D = length(x)
+    a = 20
+    b = 0.2
+    c = 2 * pi
+    sum1 = 0
+    sum2 = 0
+    for i = 1:D
+        sum1 += (x[i]^2)
+        sum2 += cos(c * x[i])
+    end
+    term1 = -a * exp(-b * sqrt(sum1 / D))
+    term2 = -exp(sum2 / D)
+    return term1 + term2 + a + exp(1)
 end
