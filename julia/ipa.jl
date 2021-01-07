868c754589e3d611af8127d79a1ccdf32f6f0878
aliwimo <aliwimo@gmail.com>
2021-01-08 00:30:38 +0300

    refactored function

--- julia/ipa.jl
+++ julia/ipa.jl
@@ -1,4 +1,5 @@
 using Distributions
+include("benchmark.jl")
 
 pop_size = 30
 dim_size = 30
@@ -10,6 +11,13 @@ UB = bound
 NoD = 1
 NoR = 1
 
+objective_function = sphere
+
+# ========================================================= #
+#                       Functions                           #
+# ========================================================= #
+
+# generating the initial population
 function generate_population()
     population = zeros(pop_size, dim_size)
     for k = 1:pop_size
@@ -18,6 +26,7 @@ function generate_population()
     return population
 end
 
+# generating just one individual
 function generate_individual()
     individual = zeros(1, dim_size)
     for i = 1:dim_size
@@ -26,92 +35,101 @@ function generate_individual()
     return individual
 end
 
+# calculating fitness of all individuals in population
 function calculate_fitnesses(population)
-    fitnesses = zeros(pop_size, 1)
+    fitnesses = zeros(pop_size)
     for i = 1:pop_size
         fitnesses[i] = fitness(population[i, :])
     end
     return fitnesses
 end
 
-function calculate_fitness(individual)
-    # here will be something
+# calculation fitness for one individual
+function fitness(individual)
+    return objective_function(individual)
 end
 
-function fitness(x)
-    sum = 0
-    for j = 1:dim_size
-        sum += (x[j] ^ 2)
-    end
-    return sum
-end
-
-function infect(x_k, x_m)
+# perform infection between two individuals
+function perform_infection(x_k, x_m)
     j = rand(1:dim_size)
     x = copy(x_k)
     x[j] = x_k[j] + (rand(Uniform(-1.0, 1.0)) * (x_k[j] - x_m[j]))
+    x[j] = check_bounds(x[j])
     return x
 end
 
-# get lists of indexes of donors and receivers
-function get_donors_receivers(fitnesses)
-    d_indexes = zeros(Int64, NoD)
-    r_indexes = zeros(Int64, NoR)
+# check if exceeded bounds
+function check_bounds(x)
+    if x > UB
+        x = UB
+    elseif x < LB
+        x = LB
+    end
+    return x
+end
+
+# get lists of indexes of doreceivers_numbers and recievers
+function get_donors_and_receivers_indexes(fitnesses)
+    donors = zeros(Int64, NoD)
+    receivers = zeros(Int64, NoR)
     sorted_indexes = sortperm(fitnesses)
     for i = 1:NoD
-        d_indexes[i] = sorted_indexes[i]
+        donors[i] = sorted_indexes[i]
     end
     reverse!(sorted_indexes)
     for i = 1:NoR
-        r_indexes[i] = sorted_indexes[i]
+        receivers[i] = sorted_indexes[i]
     end
-    return d_indexes, r_indexes
+    return donors, receivers
 end
 
-# plasma donation to infected indiviual
-function give_plasma(x_k_rcv, x_m_dnr)
+# performing plasma tranfer from donor to receiver indvidual
+function perform_plasma_transfer(receiver, donor)
     for j = 1:dim_size
-        x_k_rcv[j] += rand(Uniform(-1, 1)) * (x_k_rcv[j] - x_m_dnr[j])
+        receiver[j] += rand(Uniform(-1, 1)) * (receiver[j] - donor[j])
+        receiver[j] = check_bounds(receiver[j])
     end
-    return x_k_rcv
+    return receiver
 end
 
-# update donor with equation 5
-function update_donor(x_m_dnr)
+# updating donor's parameters
+function update_donor(donor)
     for j = 1:dim_size
-        x_m_dnr[j] += rand(Uniform(-1, 1)) * x_m_dnr[j]
+        donor[j] += rand(Uniform(-1, 1)) * donor[j]
+        donor[j] = check_bounds(donor[j])
     end
-    return x_m_dnr
+    return donor
 end
 
-function compare_with_best(x)
-    if fitness(x) < x_best_fit
-        global x_best = copy(x)
-        global x_best_fit = fitness(x)
-        # println(x_best_fit)
+# compare individual's fitness with global fitness value
+function compare_with_best_fitness(x)
+    if fitness(x) < best_fitness
+        global best_fitness = fitness(x)
     end
 end
 
+# ========================================================= #
+#                      Start of IPA                         #
+# ========================================================= #
 
+# generating initial population
 population = generate_population()
 
 # calculating fitness of population
-fitnesses = zeros(pop_size)
-for i = 1:pop_size
-    fitnesses[i] = fitness(population[i, :])
-end
+fitnesses = calculate_fitnesses(population)
 
 
-# find best individual among population
-x_best_index = argmin(fitnesses)
-x_best = copy(population[x_best_index, :])
-x_best_fit = fitnesses[x_best_index]
+# finding best individual fitness
+best_fitness = minimum(fitnesses)
 
-println("Initial Best: $x_best_fit")
+# print initial best fitness value and other statistics
+println("Initial best fitness value: $best_fitness")
+println("Number of parameters: $dim_size")
+println("Population size: $pop_size")
 
 while t_cr < t_max
 
-    # infection distirbution
+    # perform_infectionion distirbution
     for k = 1:pop_size
         if t_cr < t_max
             global t_cr += 1
@@ -121,12 +139,12 @@ while t_cr < t_max
             end
             x_k = population[k, :]
             x_m = population[m, :]
-            x_k_inf = infect(population[k, :], population[m, :])
+            x_k_inf = perform_infection(population[k, :], population[m, :])
             x_k_inf_fit = fitness(x_k_inf)
             if x_k_inf_fit < fitnesses[k]
                 population[k, :] = copy(x_k_inf)
                 fitnesses[k] = x_k_inf_fit
-                compare_with_best(x_k_inf)
+                compare_with_best_fitness(x_k_inf)
             end
         else
             break
@@ -135,7 +153,7 @@ while t_cr < t_max
 
     # plasma transfer
     dose_control = ones(Int64, NoR)
-    d_indexes, r_indexes = get_donors_receivers(fitnesses)
+    d_indexes, r_indexes = get_donors_and_receivers_indexes(fitnesses)
     treatment_control = ones(Int64, NoR)
     for i = 1:NoR
         k = r_indexes[i]
@@ -143,7 +161,7 @@ while t_cr < t_max
         while treatment_control[i] == 1
             if t_cr < t_max
                 global t_cr += 1
-                x_k_rcv_p = give_plasma(population[k, :], population[m, :])
+                x_k_rcv_p = perform_plasma_transfer(population[k, :], population[m, :])
                 x_k_rcv_p_fit = fitness(x_k_rcv_p)
                 if dose_control[i] == 1
                     if x_k_rcv_p_fit < fitnesses[m]
@@ -163,7 +181,7 @@ while t_cr < t_max
                         treatment_control[i] = 0
                     end
                 end
-                compare_with_best(population[k, :])
+                compare_with_best_fitness(population[k, :])
             else
                 break
             end
@@ -185,11 +203,11 @@ while t_cr < t_max
                 end
             end
             fitnesses[m] = fitness(population[m, :])
-            compare_with_best(population[m, :])
+            compare_with_best_fitness(population[m, :])
         else
             break
         end
     end
 end
 
-println("Best: $x_best_fit")
+println("Best: $best_fitness")
